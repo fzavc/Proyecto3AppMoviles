5ad9c2110c38a9140e9663d3d82a00bcc9967600
franc <francisca.zavalac@usm.cl>
2025-06-15 20:16:39 -0400

    Agrego detalles finales.

--- GastosPersonales/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/GastosPersonales.AssemblyInfo.cs
+++ GastosPersonales/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/GastosPersonales.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GastosPersonales")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+dc605a583f9aeb3bd1e3af86a7aed7620afa2edc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+63bb07f092e59de53377b9a3cb5d4a54fb0e5606")]
 [assembly: System.Reflection.AssemblyProductAttribute("GastosPersonales")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GastosPersonales")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
